7baa8ca09475d6cc2cee6b84cec42720e52d1d3d
Liteskarr <398501z3@gmail.com>
2020-08-25 21:54:35 +0300

    feature(UserEvent)

---
+++ userevent.py
@@ -0,0 +1,20 @@
+import pygame
+
+if not pygame.get_init():
+    pygame.init()
+
+
+class UserEvent:
+    _free_slots = []
+    _last = 1
+
+    def __init__(self):
+        if len(UserEvent._free_slots):
+            self._number = min(UserEvent._free_slots)
+            UserEvent._free_slots.remove(min(UserEvent._free_slots))
+        else:
+            self._number = UserEvent._last
+            UserEvent._last += 1
+
+    def __int__(self):
+        return self._number
